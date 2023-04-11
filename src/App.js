f8b18ce4ce9872b3b59fdaae4f1027de05b9973d
basic-coder <sakpalbhavesh2001@gmail.com>
2023-04-11 19:23:55 +0530

    frontend output window remaining

--- src/App.js
+++ src/App.js
@@ -1,53 +1,87 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { BrokenLinks } from "./api/api";
+import { UtilityResp } from "./api/api";
 
 function App() {
   const [url, setUrl] = useState("");
-  const [reports, setReports] = useState({
-    brokenLinks: false,
-    canonicalTags: false,
-    h1Tags: false,
-  });
+  const [reports, setReports] = useState("");
+  const [csvData, setcsvData] = useState([]);
+  const [header, setHeader] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [opWindow, setOpWindow] = useState(false);
 
   const handleChange = (e) => {
-    setReports((prev) => ({ ...prev, [e.target.name]: e.target.checked }));
+    setReports(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(url);
-    console.log(reports);
+    setLoading(true);
+    switch (reports) {
+      case "brokenLinks":
+        const link1 = await UtilityResp(url, "broken-link-report");
+        setcsvData(link1.data.brokenLinksReport);
+        setLoading(false);
+        setHeader(Object.keys(csvData[0]));
+        break;
+      case "canonicalTags":
+        const link2 = await UtilityResp(url, "canonical-report");
+        setcsvData(link2.data.canonicalTagReport);
+        setLoading(false);
+        setHeader(Object.keys(csvData[0]));
+        break;
+      case "h1Tags":
+        const link3 = await UtilityResp(url, "h1-report");
+        setcsvData(link3.data.h1TagReport);
+        setLoading(false);
+        setHeader(Object.keys(csvData[0]));
+        break;
+      case "canonical301":
+        const link4 = await UtilityResp(url, "canonical301");
+        setcsvData(link4.data.Canonical301);
+        setLoading(false);
+        setHeader(Object.keys(csvData[0]));
+        break;
+      case "loadingTime":
+        const link5 = await UtilityResp(url, "loading-time");
+        setcsvData(link5.data.linkLoadingTime);
+        setLoading(false);
+        setHeader(Object.keys(csvData[0]));
+        break;
 
-    setUrl("");
-    setReports({
-      brokenLinks: false,
-      canonicalTags: false,
-      h1Tags: false,
-    });
+      default:
+        console.log("nothing is selected");
+        break;
+    }
   };
 
-  const BrokenLinksArr = BrokenLinks(url);
+  const exportToCsv = (data) => {
+    const fields = Object.keys(data[0]);
 
-  const headers = Object.keys(BrokenLinksArr).map((key) => ({
-    label: key.toUpperCase(),
-    key,
-  }));
+    const csvData = [
+      fields.join(","),
+      ...data.map((obj) => fields.map((field) => obj[field]).join(",")),
+    ].join("\n");
 
-  const handleExportCSV = () => {
-    const csvData = BrokenLinksArr.map((row) =>
-      headers.map((header) => row[header]).join(",")
-    ).join("\n");
     const blob = new Blob([csvData], { type: "text/csv;charset=utf-8;" });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
-    link.setAttribute("href", url);
-    link.setAttribute("download", "data.csv");
-    link.style.visibility = "hidden";
+    link.href = url;
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let filename = `${reports}(${day}-${month}-${year}-${hours}-${min}-${sec})`;
+
+    link.setAttribute("download", filename);
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+    setcsvData([]);
   };
 
   return (
@@ -64,36 +98,92 @@ function App() {
           />
         </div>
         <div className="input-box">
-          <label>BrokenLinks</label>
+          <label htmlFor="BrokenLinks">Output Window (WIP)</label>
+          <input type="checkbox" onChange={() => setOpWindow(!opWindow)} />
+        </div>
+        <div className="input-box">
+          <label htmlFor="BrokenLinks">BrokenLinks</label>
           <input
-            type="checkbox"
-            checked={reports.brokenLinks}
-            name="brokenLinks"
+            id="BrokenLinks"
+            type="radio"
+            name="reports"
+            value="brokenLinks"
             onChange={handleChange}
           />
         </div>
         <div className="input-box">
-          <label>canonicalTags</label>
+          <label htmlFor="canonicalTags">canonicalTags</label>
           <input
-            type="checkbox"
-            checked={reports.canonicalTags}
-            name="canonicalTags"
+            id="canonicalTags"
+            type="radio"
+            name="reports"
+            value="canonicalTags"
             onChange={handleChange}
           />
         </div>
         <div className="input-box">
-          <label>h1Tags</label>
+          <label htmlFor="h1Tags">h1Tags</label>
           <input
-            type="checkbox"
-            checked={reports.h1Tags}
-            name="h1Tags"
+            id="h1Tags"
+            type="radio"
+            name="reports"
+            value="h1Tags"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-box">
+          <label htmlFor="caonical301">Canonical 301</label>
+          <input
+            id="caonical301"
+            type="radio"
+            name="reports"
+            value="canonical301"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-box">
+          <label htmlFor="loadingTime">Loading Time</label>
+          <input
+            id="loadingTime"
+            type="radio"
+            name="reports"
+            value="loadingTime"
             onChange={handleChange}
           />
         </div>
         <button type="submit">Submit</button>
-        <button onClick={handleExportCSV}>Export CSV</button>
+        {csvData.length > 0 && loading===false &&(
+          <button type="button" onClick={() => exportToCsv(csvData)}>
+            Export Data
+          </button>
+        )}
+
+        {loading && <span className="loading"></span>}
       </form>
-      <div className="output"></div>
+      {opWindow && csvData.length > 0 && (
+        <div className="output">
+          <h2> Output Window</h2>
+
+          <table>
+            <thead>
+              <tr>
+                {header.map((header) => (
+                  <th key={header}>{header.toUpperCase()}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {csvData.map((item, i) => (
+                <tr key={i}>
+                  {header.map((header) => (
+                    <td key={header}>{item[header]}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,24 @@
-import axios from "axios"
+import axios from "axios";
+const config = { headers: { "Content-type": "application/json" } };
 
-
-export const BrokenLinks = async(data) => {
+export const UtilityResp = async (url,endpoint) => {
+  let data;
   try {
-    const {url} = data
-    axios.post('http://localhost:4000/api/broken-link-report',url).then((res)=>{
-        res.json()
-        return res
-    })
+    console.log(url);
+    await axios
+      .post(
+        `http://localhost:4000/api/${endpoint}`,
+        {
+          url,
+        },
+        config
+      )
+      .then((res) => {
+        data = res;
+      });
   } catch (error) {
     console.log(error);
   }
-}
+
+  return data;
+};
